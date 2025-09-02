46a4a19229511be2e52bee62d1d0556f330abfd6
Mohammad Hafeez <syntaxhafeez@gmail.com>
2025-09-02 15:49:12 +0000

    Question_2

---
+++ Conditional/question_2.py
@@ -0,0 +1,12 @@
+# Question: Accept the gender from the user as char, and print a respective greeting message
+
+gender = input("Enter your gender: [male or female]")
+
+if gender == "male":
+    print("Good morning Sir")
+
+elif gender == "female":
+    print("Good morning Mam")
+
+else:
+    print("Good morning")
