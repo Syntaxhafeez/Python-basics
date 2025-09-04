de882e2dd724b8e7925d8155b3dc500d1a177410
Mohammad Hafeez <syntaxhafeez@gmail.com>
2025-09-04 14:59:54 +0000

    question 5

---
+++ Conditional/question_5.py
@@ -0,0 +1,9 @@
+# accept a year and check if it is a leap year or not
+
+year = int(input("Enter year: "))
+
+if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+    print(year, "is a leap year")
+
+else:
+    print(year, "is not a leap year")
